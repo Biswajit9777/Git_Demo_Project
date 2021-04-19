05d317d7aa02fba2117ac842a628c8d2a59ae97c
biswa <biswa@DESKTOP-3QKCSKL>
2021-04-19 22:49:00 +0530

    First commit

---
+++ src/test.java
@@ -0,0 +1,17 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="yes";
+		if(str=="yes"){
+			System.out.println("Eat Some Thig");
+				
+		}
+		else {
+			System.out.println("Sleep Tightly");
+		}
+
+	}
+
+}
